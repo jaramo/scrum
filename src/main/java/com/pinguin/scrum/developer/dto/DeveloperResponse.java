c27610e6438de3099b7fae3f6c0a70921239e998
Cristian Jaramillo Caceres <cristian.jaramillo@scout24.com>
2020-09-07 16:18:25 +0200

    Fix DeveloperResponse annotations Fix validations in DeveloperControllerTest

--- src/main/java/com/pinguin/scrum/developer/dto/DeveloperResponse.java
+++ src/main/java/com/pinguin/scrum/developer/dto/DeveloperResponse.java
@@ -1,35 +1,31 @@
 package com.pinguin.scrum.developer.dto;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.pinguin.scrum.developer.repository.entity.Developer;
 import com.pinguin.scrum.issue.dto.BugResponse;
 import com.pinguin.scrum.issue.dto.StoryResponse;
 
-import javax.validation.constraints.NotEmpty;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@JsonAutoDetect
 public class DeveloperResponse {
 
+    @JsonProperty("id")
     public final Long id;
 
+    @JsonProperty("name")
     public final String name;
 
+    @JsonProperty("assigned_bugs")
     public final List<BugResponse> assignedBugs;
 
+    @JsonProperty("assigned_stories")
     public final List<StoryResponse> assignedStories;
 
-    @JsonCreator
-    private DeveloperResponse(
-            @JsonProperty("id") Long id,
-            @JsonProperty("name") @NotEmpty String name,
-            @JsonProperty("assigned_bugs") List<BugResponse> assignedBugs,
-            @JsonProperty("assigned_stories") List<StoryResponse> assignedStories
-    ) {
+    private DeveloperResponse(Long id,
+                              String name,
+                              List<BugResponse> assignedBugs,
+                              List<StoryResponse> assignedStories) {
         this.id = id;
         this.name = name;
         this.assignedBugs = assignedBugs;
